f71c03ccb1d7ef7bacf06d7dfe24910411a94dd2
meafmira <o.nechiporenko@dlg.im>
2020-05-07 03:21:55 +0300

    feat: loader label change

--- src/Loader.js
+++ src/Loader.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Loader.css";
 
+const loaderLabels = [
+  "Connecting to server...",
+  "It takes more time than usual. Waking up server...",
+  "Few more seconds...",
+];
+
+const CHANGE_LABEL_INTERVAL = 5000;
+
 export function Loader() {
+  const [labelIdx, setLabelIdx] = useState(0);
+  const shouldUpdateLabel = labelIdx < loaderLabels.length - 1;
+
+  useEffect(() => {
+    if (shouldUpdateLabel) {
+      const intervalId = setInterval(() => {
+        setLabelIdx((prevLabelIdx) => prevLabelIdx + 1);
+      }, CHANGE_LABEL_INTERVAL);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [shouldUpdateLabel]);
+
   return (
     <div className="lds-container">
       <div className="lds-ring">
         <div /> <div /> <div /> <div />
       </div>
-      <div className="lds-label">Connecting to server...</div>
+      <div className="lds-label">{loaderLabels[labelIdx]}</div>
     </div>
   );
 }
